2068381a4ec6bd2c19091ee7bf0d120a84d8c3f8
emmalec <lecruitemmanuel@gmail.com>
2022-02-08 15:27:12 -0300

    agregando cambios al desafio clase 7

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Button } from "react-bootstrap";
 
 export default function ItemDetail({ item }) {
   return (
-    <Card bg="light" className="d-flex flex-row py-0">
+    <Card bg="light" className="d-flex flex-row py-4 px-4">
+      <Col>
+        <Card.Body>
+          <Card.Title>{item.title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            ARS {item.price}
+          </Card.Subtitle>
+          <Card.Text className="text-secondary">{item.description}</Card.Text>
+          <Button>Agregar al carrito</Button>
+        </Card.Body>
+      </Col>
       <Col md={5}>
         <Card.Img variant="top" src={item.pictureUrl} />
       </Col>
-      <Card.Body>
-        <Card.Title>{item.title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
-          ARS {item.price}
-        </Card.Subtitle>
-        <Card.Text className="text-secondary">{item.description}</Card.Text>
-      </Card.Body>
     </Card>
   );
 }
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,7 +6,7 @@ export default function ItemList({ products }) {
   return (
     <div>
       <CardGroup>
-        <Row xs={2} md={4} className="g-4">
+        <Row xs={2} md={4} className="g-4 mb-5">
           {products.map((item) => (
             <Item key={item.id} item={item} />
           ))}
