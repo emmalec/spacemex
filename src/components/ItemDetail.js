4a92ff55482cde2e1a4ea2df1c11cca263dfcb94
emmalec <lecruitemmanuel@gmail.com>
2022-02-08 15:33:48 -0300

    Desafio clase 7 terminado

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -4,14 +4,16 @@ import { Card, Col, Button } from "react-bootstrap";
 export default function ItemDetail({ item }) {
   return (
     <Card bg="light" className="d-flex flex-row py-4 px-4">
-      <Col>
+      <Col className="d-flex flex-column">
         <Card.Body>
           <Card.Title>{item.title}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
             ARS {item.price}
           </Card.Subtitle>
           <Card.Text className="text-secondary">{item.description}</Card.Text>
-          <Button>Agregar al carrito</Button>
+        </Card.Body>
+        <Card.Body>
+          <Button className="btn-lg">Agregar al carrito</Button>
         </Card.Body>
       </Col>
       <Col md={5}>
