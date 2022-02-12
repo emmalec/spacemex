df268c30e09475f994a3a784a8ce353e9b89c412
emmalec <lecruitemmanuel@gmail.com>
2022-02-12 12:47:02 -0300

    Terminado react-router falta arreglar greetings

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <NavBar />
-        {/* Aca se muestran los diferentes componentes de la URL */}
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ItemListContainer greeting="Hola, bienvenido a la tienda." />
-            }
-          />
-          <Route path="/producto/:itemId" element={<ItemDetailContainer />} />
-          <Route path="/category/:categoryName" />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <NavBar />
+      {/* Aca se muestran los diferentes componentes de la URL */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ItemListContainer greeting="Hola, bienvenido a la tienda." />
+          }
+        />
+        <Route path="/producto/:itemId" element={<ItemDetailContainer />} />
+        <Route
+          path="/category/:categoryName"
+          element={<ItemListContainer greeting="Nombre de la categoria" />}
+        />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import { getItems } from "../api/api";
 import ItemList from "./ItemList";
+import { useParams } from "react-router-dom";
 
 //import ItemCount from "./ItemCount";
 
@@ -10,21 +11,32 @@ export default function ItemListContainer({ greeting }) {
     console.log("Se agregaron " + count + " productos al carrito!");
   } */
 
+  const { categoryName } = useParams();
+
   const [products, setProducts] = useState([]);
 
   //aca se resuelve la promesa y se guarda el array de productos en products
   useEffect(() => {
     getItems().then(function (products) {
       //console.log(products);
-      setProducts(products);
+      // setProducts(products);
+      //aca seteo products con un filter que compara la categoria en products.category a exactamente igual que el nombre de la categoria que definimos en nuestra api y con el parametro que le pasamos en el componente padre
+      // uso un ternary operator if categoryName es null (cuando estamos en el home) nos setea en el state del hook todos los productos ELSE nos filtra por categoria
+      !categoryName
+        ? setProducts(products)
+        : setProducts(
+            products.filter((products) => {
+              return products.category === categoryName;
+            })
+          );
     });
-  }, []); //con las dependencias vacias la funcion se ejecuta una vez cargado el componente y listo
+  }, [categoryName]); //con las dependencias vacias la funcion se ejecuta una vez cargado el componente y listo
 
   return (
     <div>
       <Container>
         <Row className="py-4 text-secondary">
-          <h2>{greeting}</h2>
+          <h2>{(greeting, categoryName)}</h2>
         </Row>
         {/* <Row>
           <ItemCount stock={5} initial={1} onAdd={cartItems} />
