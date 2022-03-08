de02e91639a8bb1f1f291f0175fc76c7403202b0
emmalec <lecruitemmanuel@gmail.com>
2022-03-08 19:30:44 -0300

    validaciones con sus respectivos mensajes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -51,7 +51,7 @@ export default function Cart() {
     } else if (!/\S+@\S+\.\S+/.test(buyer.email)) {
       errors.email = "La direccion de e-mail es invalida";
     } else if (buyer.email !== buyer.emailValid) {
-      errors.email = "El e-mail no coincide";
+      errors.emailValid = "El e-mail no coincide";
     }
     setFormErrors(errors);
     //If object keys of error is 0 then true
@@ -130,7 +130,12 @@ export default function Cart() {
                     name="name"
                     value={buyer.name}
                     onChange={handleChange}
-                    className={formErrors.name && "is-invalid"}
+                    //Set class if formErrors.name exists / set invalid if true / if name length is > 1 set valid
+                    className={
+                      formErrors.name
+                        ? "is-invalid"
+                        : buyer.name.length > 0 && "is-valid"
+                    }
                   />
                   {formErrors.name && (
                     <Form.Text className="text-danger position-absolute">
@@ -146,7 +151,11 @@ export default function Cart() {
                     name="surname"
                     value={buyer.surname}
                     onChange={handleChange}
-                    className={formErrors.name && "is-invalid"}
+                    className={
+                      formErrors.surname
+                        ? "is-invalid"
+                        : buyer.surname.length > 0 && "is-valid"
+                    }
                   />
                   {formErrors.surname && (
                     <Form.Text className="text-danger position-absolute">
@@ -180,9 +189,9 @@ export default function Cart() {
                     onChange={handleChange}
                     className={formErrors.name && "is-invalid"}
                   />
-                  {formErrors.email && (
+                  {buyer.email !== buyer.emailValid && (
                     <Form.Text className="text-danger position-absolute">
-                      {formErrors.email}
+                      {formErrors.emailValid}
                     </Form.Text>
                   )}
                 </Form.Group>
